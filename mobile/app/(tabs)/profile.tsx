c53a4385d19d606133bdcec0cb16838d8224a2e0
burakorkmez <burakorkmezz@gmail.com>
2026-01-12 01:47:25 +0800

    feat: add custom hook for social authentication using Clerk SSO

---
+++ mobile/app/(tabs)/profile.tsx
@@ -0,0 +1,16 @@
+import { useAuth } from "@clerk/clerk-expo";
+import { View, Text, ScrollView, Pressable } from "react-native";
+
+const ProfileTab = () => {
+  const { signOut } = useAuth();
+  return (
+    <ScrollView className="bg-surface" contentInsetAdjustmentBehavior="automatic">
+      <Text className="text-white">Profile Tab</Text>
+      <Pressable onPress={() => signOut()} className="mt-4 bg-red-600 px-4 py-2 rounded-lg">
+        <Text>Signout</Text>
+      </Pressable>
+    </ScrollView>
+  );
+};
+
+export default ProfileTab;
